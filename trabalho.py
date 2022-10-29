a0765e18277f392d94973a15e0300fefce30f3d1
Willian Cella <willian.cella.wc@gmail.com>
2022-10-29 12:42:32 -0300

    adicionando funcoes com streamlit

--- trabalho.py
+++ trabalho.py
@@ -1,6 +1,6 @@
+import imp
 import streamlit as st
 import pandas as pd
-import numpy as np
 
 
 votacao_2022 =  pd.read_csv("./data/votacao_secao_2022_PR.csv",sep=";", encoding='latin1' )
@@ -12,25 +12,41 @@ st.write("""
 """
 )
 
-
+# Define um DF com os votos por Estados
+estados = votacao_2022['SG_UF'].unique()
+estado = st.sidebar.selectbox('Selecione o Estado', estados)
+votos_UF = votacao_2022[votacao_2022['SG_UF'] == estado]
 
 # Cria um dicionário com o código do cargo e a descrição do cargo
-cargos = votacao_2022[['CD_CARGO', 'DS_CARGO']].set_index('CD_CARGO').to_dict()
-cargos = cargos['DS_CARGO']
-cargos
+cargos_dict = votos_UF[['CD_CARGO', 'DS_CARGO']].set_index('CD_CARGO').to_dict()
+cargos_dict = cargos_dict['DS_CARGO']
+cargo = st.sidebar.selectbox('Selecione o cargo', sorted(list(cargos_dict.values())))
 
-# Seleciona o código do cargo
-cod_cargo_candidato = 3
-cargo_candidato = cargos.get(cod_cargo_candidato)
-cargo_candidato
-
-# Define um dataframe dos votos com os candidatos do cargo selecionado
-votos_cargo = votacao_2022[votacao_2022['CD_CARGO'] == cod_cargo_candidato]
+# Cria um DF com os votos dos candidatos do Estado por cargo
+votos_cargo = votos_UF[votos_UF['DS_CARGO'] == cargo]
 
 # Cria um dicionário com o número e nome dos candidatos pelo cargo selecionado
-candidatos = votos_cargo[['NR_VOTAVEL', 'NM_VOTAVEL']].set_index('NR_VOTAVEL').to_dict(orient='dict')
-candidatos = candidatos['NM_VOTAVEL']
+candidatos_dict = votos_cargo[['NR_VOTAVEL', 'NM_VOTAVEL']].set_index('NR_VOTAVEL').to_dict(orient='dict')
+candidatos_dict = candidatos_dict['NM_VOTAVEL']
+candidato = st.sidebar.selectbox('Selecione o candidato', sorted(list(candidatos_dict.values())))
 
-frame = pd.DataFrame(list(candidatos.items()),columns = ['Número votável', 'Nome candidato'])
+frame = pd.DataFrame(sorted(list(candidatos_dict.items())),columns = ['Número votável', 'Nome candidato'])
 st.dataframe(frame)
 
+# Seleciona o candidato
+cod_candidato = 0
+for codigo, candidato_ in candidatos_dict.items():
+    if candidato_ == candidato:
+        cod_candidato = codigo
+st.write(f'Candidato {candidato}, número: {cod_candidato}')
+
+# Define um DF com os votos por município
+municipios = sorted(votos_UF['NM_MUNICIPIO'].unique())
+municipio = st.selectbox('Selecione um município', municipios)
+votos_municipio = votos_UF[votos_UF['NM_MUNICIPIO'] == municipio]
+
+st.write(f'O município "{municipio}" gerou {len(votos_municipio)} votos')
+
+# Apresenta a quantidade de votos que o candidato conseguiu no município
+votos_candidato_municipio = votos_municipio[votos_municipio['NR_VOTAVEL'] == cod_candidato]
+st.write(f'O candidato "{candidato} - {cod_candidato}" conseguiu {len(votos_candidato_municipio)} em {municipio}')
